09bdcd691fc201e41e34928410d59c09deaf2699
pdmuds4 <ryosuke3191@gmail.com>
2025-01-11 14:03:53 +0900

    layout.tsxのインデントを修正

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
-import { UIProvider, ColorModeScript, extendTheme, defaultTheme } from "@yamada-ui/react";
+import { UIProvider, ColorModeScript } from "@yamada-ui/react";
 
 
 export const metadata: Metadata = {
@@ -9,19 +8,20 @@ export const metadata: Metadata = {
     description: "モンスターハンター参加型配信用、参加希望者管理アプリ",
 };
 
-
 export default function RootLayout({
     children,
-}: Readonly<{
+}: {
     children: React.ReactNode;
-}>) {
+}) {
     return (
         <html lang="en">
             <body>
-                <ColorModeScript />
-                <UIProvider>
-                    {children}
-                </UIProvider>
+                <main>
+                    <ColorModeScript />
+                    <UIProvider>
+                        {children}
+                    </UIProvider>
+                </main>
             </body>
         </html>
     );
